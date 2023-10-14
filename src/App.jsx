aeb9578ed6fcfef39839d53bae5a609440a9ed6d
inspi-writer <conservedinnovation@gmail.com>
2023-10-14 01:39:45 +0100

    push for test deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+
 import Welcome from "./pages/Welcome";
 
 function App() {
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+// import { PrivyProvider } from "@privy-io/react-auth";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+  // <PrivyProvider appId="">
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  // </PrivyProvider>
 );
