4ec2b69dbe141811731629f047969770f677dad5
inspi-writer <conservedinnovation@gmail.com>
2023-10-24 01:24:58 +0100

    airdrop page completed

--- src/pages/Airdrop.jsx
+++ src/pages/Airdrop.jsx
@@ -1,16 +1,20 @@
 import React from "react";
+import { Lightning } from "react-bootstrap-icons";
 
 const Airdrop = () => {
   return (
-    <div className="w-full flex flex-col">
+    <div className="w-full h-full flex gap-5 justify-center flex-col pl-5 pr-5">
       <div>
         <h1 className="text-3xl font-bold text-white text-center">
           Airdrop Points
         </h1>
       </div>
-      <div className="flex flex-row text-center items-center justify-center">
-        $scratch Airdrop Points are calculated usig your portfolio value,
+      <div className="flex flex-row text-center items-center text-sm justify-center text-white">
+        $scratch Airdrop Points are calculated using your portfolio value,
         verified status, referrals, and your cummulative 0xScratch score
+      </div>
+      <div className="text-sm text-center text-white">
+        Airdrop will commence in:
       </div>
       <div className="flex flex-row text-center items-center justify-center">
         <div className="flex flex-row justify-between items-center">
@@ -31,6 +35,28 @@ const Airdrop = () => {
             {/* <p className="text-white">Total Points</p> */}
           </div>
         </div>
+        <br />
+        <br />
+      </div>
+      <div className="text-sm text-center text-white">
+        0x....d6e current airdrop points:
+      </div>
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-3xl font-bold text-blue-300">0</h1>
+        {/* <p className="text-white">Total Points</p> */}
+      </div>
+      <div className="text-gray-300 text-xs text-center flex flex-col items-center">
+        <Lightning />
+        <p> Airdrop will be October 20th, 2024</p> <br />
+        <Lightning />
+        <p>
+          {" "}
+          Engagement Rewards will be boosted for all launch epoch participants
+          in perpetuity
+        </p>{" "}
+        <br />
+        <Lightning />
+        <p> 10% of total supply will be distributed</p>
       </div>
     </div>
   );
